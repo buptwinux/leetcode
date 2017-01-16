6745067fc5c9dc1ae6de62da7053de9a3860fa56
王文武 <wangwenwu@maoyan.com>
2017-01-16 17:15:20 +0800

    add two-sum

---
+++ two-sum/Solution.java
@@ -0,0 +1,39 @@
+import java.util.HashMap; 
+import java.util.terator;
+import java.util.Set;
+import java.util.Map.Entry;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>(); 
+        int [] result = {-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            hashMap.put(nums[i], i);
+        }
+        for (int j = 0; j < nums.length; j++) {
+            int res = target - nums[j];
+            if (hashMap.containsKey(res)) {
+                int temp = hashMap.get(res);
+                if (temp != j) {
+                    result[0] = j;
+                    result[1] = temp;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    } 
+
+    public static void main (String [] args) {
+        int [] a = {3, 2, 4};
+        int t = 6;
+        int [] res = new int[2];
+        Solution so = new Solution();
+        res = so.twoSum(a, t);
+
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
+    }
+}
